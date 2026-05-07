43b4c3650fc682dcf7946bd0006f4fe67b05e05e
seronsenapati <seronsenapati@example.com>
2026-05-07 12:25:39 +0530

    Add solution for 61 Rotate List

---
+++ 61_rotate_list.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0){
+            return head;
+        }
+
+        ListNode temp = head;
+        int n = 1;
+        while(temp.next != null){
+            temp = temp.next;
+            n++;
+        }
+
+
+        k = k % n;
+        if(k == 0){
+            return head;
+        }
+        temp.next = head;
+        int stepToNewTail = n - k;
+
+        ListNode newTail = head;
+        for(int i = 1 ; i < stepToNewTail ; i++){
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
