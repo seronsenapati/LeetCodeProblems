9339c0585a2a3fb36426c420158e97dbee7a3df7
seronsenapati <seronsenapati@example.com>
2026-05-09 21:23:30 +0530

    Update majority element solution

--- 0001-1000/169_majority_element.java
+++ 0001-1000/169_majority_element.java
@@ -1,8 +1,32 @@
-import java.util.Arrays;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int n = nums.length;
+
+        int count = 0;
+        int el = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(count == 0){
+                count = 1;
+                el = nums[i];
+            }else if(el == nums[i]){
+                count++;;
+            }else{
+                count--;
+            }
+        }
+
+        int count1 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == el){
+                count1++; 
+            }
+        }
+
+        if(count1 > (n/2)){
+            return el;
+        }
+
+        return -1;
     }
 }
