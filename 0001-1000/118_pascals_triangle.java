d4e042d3715fa45857aeb98eec9d15824a789847
seronsenapati <seronsenapati@example.com>
2026-05-12 14:57:26 +0530

    Add solution for LeetCode 118 pascals triangle

---
+++ 0001-1000/118_pascals_triangle.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static List<Integer> generateRow(int row) {
+        int ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / (col);
+            ansRow.add(ans);
+        }
+        return ansRow;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            ans.add(generateRow(i));
+        }
+        return ans;
+    }
+}
