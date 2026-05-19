b253ded4a2f02242516ecba2d488af9a91ee15f0
seronsenapati <seronsenapati@example.com>
2026-05-19 10:42:57 +0530

    Add problem 2540: Minimum Common Value

---
+++ 2001-3000/2540_minimum_common_value.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0 , j = 0;
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        while(i < n && j < m){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                return nums1[i];
+            }
+        }
+
+        return -1;
+    }
+}
