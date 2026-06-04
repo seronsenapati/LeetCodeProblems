90175e298ac6934abe368f65bada6a316b1e8982
seronsenapati <seronsenapati@gmail.com>
2026-06-04 22:46:49 +0530

    add #1752

--- 1001-2000/1752_check_if_array_is_sorted_and_rotated.java
+++ 1001-2000/1752_check_if_array_is_sorted_and_rotated.java
@@ -3,8 +3,17 @@ class Solution {
         int n = nums.length;
         int count = 0 ; 
         for(int i=0 ; i<n ; i++){
-            if(nums[i] > nums[(i+1) % n]){
+            if(nums[i] > nums[(i+1) % n]){ // allows circular checking 
                 count++;
+
+                /*
+                if arr = [3,4,5,1,2]
+                3 > 4 ? No
+                4 > 5 ? No
+                5 > 1 ? Yes  count=1
+                1 > 2 ? No
+                2 > 3 ? No
+                */
             }
         }
         return count <= 1;
