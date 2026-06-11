e8e351fa99fb8b08aebb8cf2e26b8cfa4a2442ce
seronsenapati <seronsenapati@gmail.com>
2026-06-12 00:06:21 +0530

    Add solution for 3121

---
+++ 3001-4000/3121_count_the_number_of_special_characters_ii.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+
+        int[] lower = new int[26];
+        int[] upper = new int[26];
+        Arrays.fill(lower, -1);
+        Arrays.fill(upper, -1);
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                lower[ch - 'a'] = i;
+            } else {
+                if (upper[ch - 'A'] == -1)
+                    upper[ch - 'A'] = i;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i])
+                count++;
+        }
+
+        return count;
+    }
+}
